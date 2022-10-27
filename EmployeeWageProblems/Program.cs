3c98175322474c2d5da52773868bef157ba2c5d6
lalitraghu1c <raghuwanshilalit5@gmail.com>
2022-10-27 18:31:00 +0530

    [Lalit] Add: Folder Structure Created

--- EmployeeWageProblems/Program.cs
+++ EmployeeWageProblems/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace EmployeeWageProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Problems Statement");
+        }
+    }
+}
